adde860fd6beb234d3eace0ec8d82042e78d6242
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-02-23 16:14:55 +0100

    refactored the arrays by giving them all id, then created a remove by id action

--- src/main/webapp/domManipulationScript.js
+++ src/main/webapp/domManipulationScript.js
@@ -1,6 +1,16 @@
-let boys = ["Peter", "lars", "Ole"]
-let girls = ["Janne", "hanne", "Sanne"]
-let all = [...boys, ...girls]
+// let boys = [ "Peter", "lars", "Ole" ]
+// let girls = [ "Janne", "hanne", "Sanne" ]
+let boys = [
+  { id: "" + Math.random(), name: "Peter" },
+  { id: "" + Math.random(), name: "lars" },
+  { id: "" + Math.random(), name: "Ole" }
+]
+let girls = [
+  { id: Math.random(), name: "Janne" },
+  { id: Math.random(), name: "hanne" },
+  { id: Math.random(), name: "Sanne" }
+]
+let all = [ ...boys, ...girls ]
 
 const showBoysNode = document.getElementById("show-boys")
 const showGirlsNode = document.getElementById("show-girls")
@@ -8,48 +18,90 @@ const showAllNode = document.getElementById("show-all")
 
 //Called to update the current display of boys
 function displayAllBoys(boys) {
-  showBoysNode.innerHTML = boys.map(boy => `<p>${boy}</p>`).join("")
+  showBoysNode.innerHTML = boys.map(
+      boy => `<div style="display: flex; justify-content: center">
+                <p>${boy.name}</p>
+                <button id="${boy.id}">x</button>
+              </div>`).join("")
+}
+
+//Remove by boy by id
+function createBoyOnClicks() {
+  boys.forEach(boy => {
+    const boyButton = document.getElementById(boy.id);
+    boyButton.addEventListener("click", () => {
+      boysReducer({ type: boyActions.REMOVE_BY_ID, payload: boy.id })
+    })
+  })
 }
 
 //Called to update the current display of girls
 function displayAllGirls(girls) {
-  showGirlsNode.innerHTML = girls.map(girl => `<p>${girl}</p>`).join("")
+  showGirlsNode.innerHTML = girls.map(
+      girl => `<div style="display: flex; justify-content: center">
+                <p>${girl.name}</p>
+                <button id="${girl.id}">x</button>
+              </div>`).join("")
+}
+
+//Remove girl by id
+function createGirlOnClicks() {
+  girls.forEach(girl => {
+    const girlButton = document.getElementById(girl.id);
+    girlButton.addEventListener("click", () => {
+      girlsReducer({ type: girlActions.REMOVE_BY_ID, payload: girl.id })
+    })
+  })
 }
 
 //Called to update the current display of all the people
 function displayAll(all) {
-  showAllNode.innerHTML = all.map(person => `<p>${person}</p>`).join("")
+  showAllNode.innerHTML = all.map(person => `<p>${person.name}</p>`).join("")
 }
 
 displayAllBoys(boys)
 displayAllGirls(girls)
 displayAll(all)
+createBoyOnClicks()
+createGirlOnClicks()
 
 const boyActions = {
   ADD_BOY: "ADD_BOY",
   REMOVE_LAST_BOY: "REMOVE_LAST_BOY",
   REMOVE_FIRST_BOY: "REMOVE_FIRST_BOY",
+  REMOVE_BY_ID: "REMOVE_BY_ID"
 }
+
 //Responsible for all of the actions related to boys
 function boysReducer(action) {
   switch (action.type) {
     case boyActions.ADD_BOY:
-      boys = [...boys, action.payload]
-      all = [...boys, ...girls]
+      boys = [ ...boys, { id: Math.random(), name: action.payload } ]
+      all = [ ...boys, ...girls ]
       displayAllBoys(boys)
       displayAll(all)
+      createBoyOnClicks()
       break
     case boyActions.REMOVE_FIRST_BOY:
       boys.shift()
-      all = [...boys, ...girls]
+      all = [ ...boys, ...girls ]
       displayAllBoys(boys)
       displayAll(all)
+      createBoyOnClicks()
       break
     case boyActions.REMOVE_LAST_BOY:
       boys.pop()
-      all = [...boys, ...girls]
+      all = [ ...boys, ...girls ]
       displayAllBoys(boys)
       displayAll(all)
+      createBoyOnClicks()
+      break
+    case boyActions.REMOVE_BY_ID:
+      boys = boys.filter(boy => boy.id !== action.payload)
+      all = [ ...boys, ...girls ]
+      displayAllBoys(boys)
+      displayAll(all)
+      createBoyOnClicks()
       break
     default:
       throw new Error("Unknown action: " + action.type)
@@ -60,27 +112,39 @@ const girlActions = {
   ADD_GIRL: "ADD_GIRL",
   REMOVE_LAST_GIRL: "REMOVE_LAST_GIRL",
   REMOVE_FIRST_GIRL: "REMOVE_FIRST_GIRL",
+  REMOVE_BY_ID: "REMOVE_BY_ID"
 }
+
 //Responsible for all of the actions related to girls
 function girlsReducer(action) {
-  switch(action.type) {
+  switch (action.type) {
     case girlActions.ADD_GIRL:
-      girls = [...girls, action.payload]
-      all = [...boys, ...girls]
+      girls = [ ...girls, { id: Math.random(), name: action.payload } ]
+      all = [ ...boys, ...girls ]
       displayAllGirls(girls)
       displayAll(all)
+      createGirlOnClicks()
       break
     case girlActions.REMOVE_FIRST_GIRL:
       girls.shift()
-      all = [...boys, ...girls]
+      all = [ ...boys, ...girls ]
       displayAllGirls(girls)
       displayAll(all)
+      createGirlOnClicks()
       break
     case girlActions.REMOVE_LAST_GIRL:
       girls.pop()
-      all = [...boys, ...girls]
+      all = [ ...boys, ...girls ]
       displayAllGirls(girls)
       displayAll(all)
+      createGirlOnClicks()
+      break
+    case girlActions.REMOVE_BY_ID:
+      girls = girls.filter(girl => girl.id !== action.payload)
+      all = [ ...boys, ...girls ]
+      displayAllGirls(girls)
+      displayAll(all)
+      createGirlOnClicks()
       break
     default:
       throw new Error("Unknown action: " + action.type)
@@ -94,7 +158,7 @@ const addBoyFormNode = document.getElementById("add-boy-form")
 addBoyFormNode.addEventListener("submit", (e) => {
   e.preventDefault()
   const boyInputValue = boyInputNode.value
-  boysReducer({type: boyActions.ADD_BOY, payload: boyInputValue})
+  boysReducer({ type: boyActions.ADD_BOY, payload: boyInputValue })
   boyInputNode.value = ""
 })
 
@@ -105,7 +169,7 @@ const addGirlFormNode = document.getElementById("add-girl-form")
 addGirlFormNode.addEventListener("submit", (e) => {
   e.preventDefault()
   const girlInputValue = girlInputNode.value
-  girlsReducer({type: girlActions.ADD_GIRL, payload: girlInputValue})
+  girlsReducer({ type: girlActions.ADD_GIRL, payload: girlInputValue })
   girlInputNode.value = ""
 })
 
@@ -113,13 +177,12 @@ const removeBoyButtonNode = document.getElementById("remove-boy")
 
 //Remove boy click
 removeBoyButtonNode.addEventListener("click", () => {
-  const checkedValue = document.querySelector("input[name='remove-selection']:checked").value;
+  const checkedValue = document.querySelector(
+      "input[name='remove-selection']:checked").value;
   if (checkedValue === "first") {
-    console.log(checkedValue)
-    boysReducer({type: boyActions.REMOVE_FIRST_BOY})
+    boysReducer({ type: boyActions.REMOVE_FIRST_BOY })
   } else if (checkedValue === "last") {
-    console.log(checkedValue)
-    boysReducer({type: boyActions.REMOVE_LAST_BOY})
+    boysReducer({ type: boyActions.REMOVE_LAST_BOY })
   }
 })
 
@@ -127,11 +190,12 @@ const removeGirlButtonNode = document.getElementById("remove-girl")
 
 //Remove girl click
 removeGirlButtonNode.addEventListener("click", () => {
-  const checkedValue = document.querySelector("input[name='remove-selection']:checked").value;
+  const checkedValue = document.querySelector(
+      "input[name='remove-selection']:checked").value;
   if (checkedValue === "first") {
-    girlsReducer({type: girlActions.REMOVE_FIRST_GIRL})
+    girlsReducer({ type: girlActions.REMOVE_FIRST_GIRL })
   } else if (checkedValue === "last") {
-    girlsReducer({type: girlActions.REMOVE_LAST_GIRL})
+    girlsReducer({ type: girlActions.REMOVE_LAST_GIRL })
   }
 })
 
@@ -147,5 +211,7 @@ const sortAllButtonNode = document.getElementById("sort-all-button")
 
 //Sort all click
 sortAllButtonNode.addEventListener("click", () => {
-  displayAll([...boys, ...girls])
+  displayAll([ ...boys, ...girls ])
 })
+
+
