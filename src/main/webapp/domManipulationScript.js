4633b66c3f7498474bf9dfe2b0d9f5797fab6c07
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-02-23 14:14:28 +0100

    add boy and add girl is now working and updating correctly

--- src/main/webapp/domManipulationScript.js
+++ src/main/webapp/domManipulationScript.js
@@ -6,14 +6,17 @@ const showBoysNode = document.getElementById("show-boys")
 const showGirlsNode = document.getElementById("show-girls")
 const showAllNode = document.getElementById("show-all")
 
+//Called to update the current display of boys
 function displayAllBoys(boys) {
   showBoysNode.innerHTML = boys.map(boy => `<p>${boy}</p>`).join("")
 }
 
+//Called to update the current display of girls
 function displayAllGirls(girls) {
   showGirlsNode.innerHTML = girls.map(girl => `<p>${girl}</p>`).join("")
 }
 
+//Called to update the current display of all the people
 function displayAll(all) {
   showAllNode.innerHTML = all.map(person => `<p>${person}</p>`).join("")
 }
@@ -22,12 +25,58 @@ displayAllBoys(boys)
 displayAllGirls(girls)
 displayAll(all)
 
+const boyActions = {
+  ADD_BOY: "ADD_BOY"
+}
+//Responsible for all of the actions related to boys
 function boysReducer(action) {
   switch (action.type) {
-    case "ADD_BOY":
-      return
+    case boyActions.ADD_BOY:
+      boys = [...boys, action.payload]
+      all = [...boys, ...girls]
+      displayAllBoys(boys)
+      displayAll(all)
+      break
     default:
       throw new Error("Unknown action: " + action.type)
   }
 }
 
+const girlActions = {
+  ADD_GIRL: "ADD_GIRL"
+}
+//Responsible for all of the actions related to girls
+function girlsReducer(action) {
+  switch(action.type) {
+    case girlActions.ADD_GIRL:
+      girls = [...girls, action.payload]
+      all = [...boys, ...girls]
+      displayAllGirls(girls)
+      displayAll(all)
+      break
+    default:
+      throw new Error("Unknown action: " + action.type)
+  }
+}
+
+const boyInputNode = document.getElementById("boy-input")
+const addBoyFormNode = document.getElementById("add-boy-form")
+
+//Add boy form submit
+addBoyFormNode.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const boyInputValue = boyInputNode.value
+  boysReducer({type: boyActions.ADD_BOY, payload: boyInputValue})
+  boyInputNode.value = ""
+})
+
+const girlInputNode = document.getElementById("girl-input")
+const addGirlFormNode = document.getElementById("add-girl-form")
+
+//Add girl form submit
+addGirlFormNode.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const girlInputValue = girlInputNode.value
+  girlsReducer({type: girlActions.ADD_GIRL, payload: girlInputValue})
+  girlInputNode.value = ""
+})
