0bd8a92d33ee71ce527023cbbbec94938d593ea2
jensgelbek <jensgelbek@gmail.com>
2021-02-24 15:46:34 +0100

    getalll restendpoint

--- src/main/webapp/joke.js
+++ src/main/webapp/joke.js
@@ -8,3 +8,26 @@ document.getElementById("form").addEventListener("submit", function (event) {
 });
 
 
+ 
+    
+   
+   function getAll() {
+    const DATA = fetch(URL+"all")
+            .then((res) => res.json())
+            .then(jokes => {
+                const tableRows = jokes.map(joke => {
+                    const row = `<tr>
+                        <td> ${joke.id} </td> 
+                        <td> ${joke.theJoke} </td>
+               <td> ${joke.reference} </td>
+               <td> ${joke.type} </td>
+               <td> ${joke.rating} </td>
+                        </tr>`;
+                    return row;
+                });
+            let tableAsString = "<tr><th>id</th><th>The Joke</th><th>reference</th><th>Type</th><th>Rating</th></tr>".concat(tableRows.join("  "));
+            return document.getElementById("jokes").innerHTML = tableAsString;
+    });
+}
+
+getAll();
